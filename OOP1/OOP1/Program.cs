57646592d4b1176195f36dd82dae59e06a7a32d1
Michael Yearsley <59950440+michaelyearsley@users.noreply.github.com>
2020-03-09 23:06:56 -0700

    Add files via upload

---
+++ OOP1/OOP1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        public static string title = "C# Essential Training";
+        public static int whichpart;
+        static void Main(string[] args)
+        {
+            whichpart = 2;
+            var message = string.Format("Welcome to {0} Part {1}!", title, whichpart);
+            Console.WriteLine(message);
+            Console.WriteLine("Press the enter key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
