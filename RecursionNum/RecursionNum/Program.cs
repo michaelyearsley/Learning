a2401800a2d3991e95d230beef9c7b761a1c1784
Michael Yearsley <59950440+michaelyearsley@users.noreply.github.com>
2020-03-30 20:10:46 -0700

    Add files via upload

---
+++ RecursionNum/RecursionNum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace RecursionNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number Separator");
+            numSep(231);
+            Console.WriteLine("Factorial");
+            Console.WriteLine(factorial(3));
+        }
+        public static void numSep(int num)
+        {
+
+            if(num < 10)
+            {
+                Console.WriteLine(num);
+                return;
+            }
+            numSep(num / 10);
+            Console.WriteLine(num % 10);
+        }
+        public static long factorial(int num)
+        {
+            if (num < 0)
+            {
+                throw new Exception(" You can't have a negative");
+            }
+            else if (num == 0)
+            {
+                return 1;
+
+            }
+            return num * factorial(num - 1);
+        }
+
+
+    }
+}
