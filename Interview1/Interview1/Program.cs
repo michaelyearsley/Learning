8f46037ee0757a22404f0831bed5db52e16af645
Michael Yearsley <59950440+michaelyearsley@users.noreply.github.com>
2020-04-29 22:11:15 -0700

    Add files via upload

---
+++ Interview1/Interview1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Interview1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 0, 0, 0, 0, 25, 25, 25, 7, 5, 6, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 44, 33, 22, 22, 3, 4, 4, 1, 8, 9, 6 };
+            RLE(arr);
+        }
+        public static void RLE(int[] num)
+        {
+            List<int> NewRLE = new List<int>();
+            int count = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                
+                if (i == 0)
+                {
+                    count++;
+                }
+                else if (i == num.Length-1 && num[i] != num[i - 1])
+                {
+                    NewRLE.Add(1);
+                    NewRLE.Add(num[i]);
+                }
+                else if (num[i] == num[i - 1])
+                {
+                    count++;
+                  
+                }
+                else if (num[i] != num[i - 1])
+                {
+                    NewRLE.Add(count);
+                    NewRLE.Add(num[i - 1]);
+                    count = 1;
+                }
+            }
+            Console.WriteLine(String.Join(",", NewRLE));
+
+        }
+    }
+}
